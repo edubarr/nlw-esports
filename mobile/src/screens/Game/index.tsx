e24b7a3dda71f85efb4f765171b03099f00e9ddf
edubarr <eduaraujobarros@gmail.com>
2022-09-18 16:10:39 -0300

    aula 5 - finish

--- mobile/src/screens/Game/index.tsx
+++ mobile/src/screens/Game/index.tsx
@@ -11,9 +11,11 @@ import { THEME } from "../../theme";
 import { Heading } from "../../components/Heading";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
 import { useEffect, useState } from "react";
+import { DuoMatch } from "../../components/DuoMatch";
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [duoSelected, setDuoSelected] = useState("");
 
   const navigation = useNavigation();
   const route = useRoute();
@@ -21,6 +23,12 @@ export function Game() {
 
   function handleGoBack() {
     navigation.goBack();
+  }
+
+  async function getDiscordUser(adId: string) {
+    fetch(`http://192.168.88.20:3333/ads/${adId}/discord`).then((response) =>
+      response.json().then((data) => setDuoSelected(data.discord))
+    );
   }
 
   useEffect(() => {
@@ -61,7 +69,7 @@ export function Game() {
           data={duos}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <DuoCard data={item} onConnect={() => {}} />
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
           )}
           horizontal
           style={styles.containerList}
@@ -74,6 +82,12 @@ export function Game() {
               Não há anúncios publicados ainda.
             </Text>
           )}
+        />
+
+        <DuoMatch
+          discord={duoSelected}
+          visible={duoSelected.length > 0}
+          onClose={() => setDuoSelected("")}
         />
       </SafeAreaView>
     </Background>
